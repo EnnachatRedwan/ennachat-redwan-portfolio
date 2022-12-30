3a1ccd26c88c445b4189eb90e982058420f9d6d7
EnnachatRedwan <100237071+EnnachatRedwan@users.noreply.github.com>
2022-12-30 18:50:23 +0100

    final for today

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -9,21 +9,86 @@ const Education = (props) => {
       <div className="container">
         <SectionHeader
           title={
-            <i
-              className="fa-solid fa-graduation-cap"
-              style={{ color: "#6871d7" }}
-            ></i>
+            <h1 className="display-4">
+              <i
+                className="fa-solid fa-graduation-cap"
+                style={{ color: "#6871d7" }}
+              ></i>
+            </h1>
           }
           style={{ color: "#6871d7" }}
           subtitle="My Education"
         />
-        <div className="row">
-          <div className="col col-md-3">
-            <div className="card">
-              <div className="card-body">
-                <h1 className="text-center">
-                  <i class="fa-solid fa-file-certificate"></i>
-                </h1>
+        <div className="row py-5 justify-content-around g-4">
+          <div className="col-lg-3">
+            <div
+              className="card"
+              style={{
+                background: "#f7f7ff",
+                color: "#6871d7",
+                boxShadow: "0 0 15px #4441",
+                border: "2px #6871d733 solid",
+                borderRadius: "20px",
+              }}
+            >
+              <div className="card-body text-center d-grid gap-3">
+                <div className="display-6">
+                  <i class="fa-sharp fa-solid fa-certificate"></i>
+                </div>
+                <h1 className="card-title h3">High School Degree</h1>
+                <p className="card-text h6">
+                  "Baccalauréat" equivalent to high school degree in the USA
+                </p>
+                <p>Physics and chemistry option</p>
+                <h3 className="h5">Obtained in 2019</h3>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3">
+            <div
+              className="card"
+              style={{
+                background: "#f7f7ff",
+                color: "#6871d7",
+                boxShadow: "0 0 15px #4441",
+                border: "2px #6871d733 solid",
+                borderRadius: "20px",
+              }}
+            >
+              <div className="card-body text-center d-grid gap-3">
+                <div className="display-6">
+                  <i class="fa-sharp fa-solid fa-certificate"></i>
+                </div>
+                <h1 className="card-title h3">High School Degree</h1>
+                <p className="card-text h6">
+                  "Baccalauréat" equivalent to high school degree in the USA
+                </p>
+                <p>Physics and chemistry option</p>
+                <h3 className="h5">Obtained in 2019</h3>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-3">
+            <div
+              className="card"
+              style={{
+                background: "#f7f7ff",
+                color: "#6871d7",
+                boxShadow: "0 0 15px #4441",
+                border: "2px #6871d733 solid",
+                borderRadius: "20px",
+              }}
+            >
+              <div className="card-body text-center d-grid gap-3">
+                <div className="display-6">
+                  <i class="fa-sharp fa-solid fa-certificate"></i>
+                </div>
+                <h1 className="card-title h3">High School Degree</h1>
+                <p className="card-text h6">
+                  "Baccalauréat" equivalent to high school degree in the USA
+                </p>
+                <p>Physics and chemistry option</p>
+                <h3 className="h5">Obtained in 2019</h3>
               </div>
             </div>
           </div>
