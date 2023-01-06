f2ebf5bed7a0a6c40eefc873ecfa072e9f983b34
EnnachatRedwan <100237071+EnnachatRedwan@users.noreply.github.com>
2023-01-06 13:33:07 +0100

    fixed all project card issues

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,14 +5,26 @@ import ProjectCard from "../../ui/ProjectCard";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Projects = (props) => {
+  const _projects = [
+    { title: "Full projects manager", img: "/prj1.jpg", code: "https://github.com/EnnachatRedwan/pfe_project_tracking_ennachat_redwan_flutter", demo: "" },
+  ];
+
   return (
     <Section id="projects">
       <div className="container">
         <SectionHeader title="Projects" subtitle="My Projects" />
         <div className="row py-5">
-          <div className="col-md-4">
-            <ProjectCard title="Full projects manager" />
-          </div>
+          {_projects.map((prj, i) => (
+            <div className="col-md-4">
+              <ProjectCard
+                key={i}
+                title={prj.title}
+                img={prj.img}
+                code={prj.code}
+                demo={prj.demo}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </Section>
--- src/ui/ProjectCard.jsx
+++ src/ui/ProjectCard.jsx
@@ -3,16 +3,20 @@ import classes from "./projectCard.module.css";
 const ProjectCard = (props) => {
   return (
     <div className={classes["card-container"]}>
-      <img src="/prj1.jpg" alt="project picture" />
+      <img src={props.img} alt="project picture" />
       <div className={classes["filter"]} />
       <h1>{props.title}</h1>
       <div className={classes["controls"]}>
-        <a href={props.code} className={classes["card-btn"]}>
-          <i class="fa-solid fa-code"></i>
-        </a>
-        <a href={props.demo} className={classes["card-btn"]}>
-          Demo
-        </a>
+        {props.code && (
+          <a href={props.code} target="_blank" className={classes["card-btn"]}>
+            <i class="fa-solid fa-code"></i>
+          </a>
+        )}
+        {props.demo && (
+          <a href={props.demo} target="_blank" className={classes["card-btn"]}>
+            Demo
+          </a>
+        )}
       </div>
     </div>
   );
